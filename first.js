36ea1149b2a0702b02eee27227eba3693abf7a0e
Isha Choudhary <isha.25b01011372@abes.ac.in>
2026-05-28 22:51:42 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,19 @@
+console.log("hello amarjeet!");
+console.log("how are you?");
+let fullName="amarjeet kushwaha";
+ let age=25;
+a=null;
+//float avilue
+price =98.55;
+y=undefined;
+isFollow=true;
+console.log(fullName);
+console.log(age);
+const student={
+    name: "amarjeet",
+    roll: 15,
+    cgpa: 8.6,
+    ispass: true,
+};
+console.log(student["roll"]);
+// student["name"]="kushwaha";
---
+++ loop.js
@@ -0,0 +1,22 @@
+//using for loop
+var sum=0;
+for(let i=0;i<100;i++){
+    sum=sum+i;
+}
+console.log("sum :",sum);
+//using while loop
+let x=5;
+while(x>0){
+    console.log("amarjeet:");
+    x--;
+}
+// using do-while loop
+let xx=1;
+do{
+    console.log("amarjeet kushwaha:");
+     xx--;
+}
+while(xx>0);
+
+
+
